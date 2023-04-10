b80b437f1ac928cd2ffcabc47b9ce636dc7d6d54
yong park <yong.park123456@gmail.com>
2023-04-10 13:07:57 -0600

    ya funciona el put de datos

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import os
 
 
 #lo que busco es que al correr mi main y ingresar put "employees", "Geoffrey", "personal_data:age", 32, lo que haga es que primero encuentre si existe un archivo llamdo employees.json y luego de ello en el cree un rowkey que seria Geoffrey, luego revise si existe en el json el personal_data que en este caso si existe dentro de column families y entonces haga que se agregue el age y luego 32 para que al final el json quede algo asi.  {"table_name": "employees", "column_families": ["\"personal_data\"", "\"profesional_data\""]}
+data_dir = "data"
 
 def submit_text():
     command = entry.get()
@@ -16,8 +17,9 @@ def submit_text():
         
         # print(args)
         table_name = command_parts[0].strip().replace('create ', '').replace('"', '')
-        # print("table_name: ", table_name)
-        columns = [col.strip() for col in command_parts[1:]]
+        columns = [col.strip().replace('"', '') for col in command_parts[1:]]
+        print("table_name: ", table_name)
+        print("columns: ", columns)
         create_table(table_name, columns)
         output.config(text=f'Table {table_name} created with columns {columns}')
         
@@ -25,78 +27,58 @@ def submit_text():
         print(command_parts)
         table_name = command_parts[0].strip().replace('put ', '').replace('"', '')
         row_key = command_parts[1].strip().replace('"', '')
-        column = command_parts[2].split(":")[0].strip().replace('"', '')
-        new_value = command_parts[2].split(":")[1].strip().replace('"', '')
-        value = command_parts[3].strip()
+        column = command_parts[2].strip().replace('"', '')
+        value = command_parts[3].strip().replace('"', '')
         print("table_name: ", table_name)
         print("row_key: ", row_key)
         print("column: ", column)
-        print("new_value: ",new_value)
         print("value: ", value)
     
-        put_data(table_name, column, value)
-        output.config(text = f'Row added to table "{table_name}" with row_key {row_key} and column {column} added it variable {new_value} set to {value}\n')
+        put_data(table_name, row_key, column, value)
+        output.config(text = f'Row added to table "{table_name}" with row_key {row_key} and column {column} set to {value}\n')
         
     else:
         output.config(text="Comando no reconocido")
 
-# Función para crear la tabla
 def create_table(table_name, columns):
-    
-    table = {
-        "table_name": table_name,
-        "column_families": columns
-    }
+    table_data = {"columns": columns, "rows": {}}
+    if not os.path.exists(data_dir):
+        os.makedirs('data')
+    table_file = os.path.join(data_dir, f"{table_name}.json")
+    with open(table_file, "w") as f:
+        json.dump(table_data, f)
+    print(f"Table '{table_name}' created with columns: {columns}")
 
-    # Crear archivo JSON con la información de la tabla
-    with open(f"{table_name}.json", "w") as f:
-        json.dump(table, f)
 
-    print(f"Tabla {table_name} creada con éxito.")
-    
-    # table = {"row_key": []}
-    # for column in columns:
-    #     table[column] = []
-    # with open(f'{table_name}.json', 'w') as f:
-    #     json.dump(table, f)
-    # print(f'Table {table_name} created with columns: {columns}')
-    
-    # table = {1: {col: '' for col in columns}}
-    # # Eliminar las comillas del nombre de la tabla
-    # table_name = table_name.replace('"', '')
-    # with open(f'{table_name}.json', 'w') as f:
-    #     json.dump(table, f)
-
-#funcion del put
-def put_data(table_name, column, value):
-    # Leer información de la tabla desde el archivo JSON
-    with open(f"{table_name}.json", "r") as f:
-        table_info = json.load(f)
-
-    # Extraer el nombre de la column family y la columna
-    column_family, column_name = column.split(":")
-    print("column_family: ", column_family)
-    print("column_name: ", column_name)
-    # Verificar si la column family existe
-    if column_family not in table_info["column_families"]:
-        print(f"Error: la column family '{column_family}' no existe en la tabla.")
+def put_data(table_name, row_key, column, value):
+    table_file = os.path.join(data_dir, f"{table_name}.json")
+    if not os.path.exists(table_file):
+        print(f"Table '{table_name}' does not exist.")
         return
 
-    # Leer datos existentes del archivo JSON
-    try:
-        with open(f"{table_name}/{column_family}/{table_name}.json", "r") as f:
-            data = json.load(f)
-    except FileNotFoundError:
-        data = {}
+    with open(table_file, "r") as f:
+        table_data = json.load(f)
 
-    # Agregar valor a la columna correspondiente
-    data.setdefault(table_name, {}).setdefault(column_family, {})[column_name] = value
+    print(table_data)
+    print(table_data["columns"])
+    column_family, qualifier = column.split(":")
+    if column_family not in table_data["columns"]:
+        print(f"Column '{column}' does not exist in table '{table_name}'.")
+        return
 
-    # Escribir datos actualizados en el archivo JSON
-    with open(f"{table_name}/{column_family}/{table_name}.json", "w") as f:
-        json.dump(data, f)
+    if row_key not in table_data["rows"]:
+        table_data["rows"][row_key] = {}
 
-    print(f"Valor '{value}' agregado a la columna '{column}' de la fila '{table_name}' con éxito.")
+    if column_family not in table_data["rows"][row_key]:
+        table_data["rows"][row_key][column_family] = {}
+
+    table_data["rows"][row_key][column_family][qualifier] = str(value)
+
+    with open(table_file, "w") as f:
+        json.dump(table_data, f)
+
+    print(f"Data added to table '{table_name}' for row '{row_key}', column '{column}'.")
+
 
 
 
